3e07adfb4ed54ee493fc2f8c3dbf3a93a0bc8ba6
Dean Chuang <azurenokite@gmail.com>
2016-01-03 21:50:04 +0000

    add google maps api

--- app/app.js
+++ app/app.js
@@ -18,4 +18,28 @@ $(document).ready(function() {
     });
   });
   
+  
+  function initialize() {
+    var myCenter=new google.maps.LatLng(33.759753,-117.991321);
+    var mapProp = {
+      center: myCenter,
+      zoom: 11,
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker=new google.maps.Marker({
+      position:myCenter,
+      animation:google.maps.Animation.BOUNCE
+      });
+    google.maps.event.addListener(map, 'click', function(event){
+          this.setOptions({scrollwheel:true});
+      });
+    marker.setMap(map);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+  
+  
+  
 });
+
